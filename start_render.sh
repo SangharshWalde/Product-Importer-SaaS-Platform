0f278b28b1291b06197e37b676cdc9096ce7b5b9
Sangharsh Walde <sangharshwalde2701@gmail.com>
2025-11-29 00:50:55 +0530

    Add proxy headers for Render

--- start_render.sh
+++ start_render.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Celery Worker in background
-celery -A celery_app worker --loglevel=info &
+celery -A celery_app worker --loglevel=info --concurrency=1 &
 
 # Start Web Server
-uvicorn app:app --host 0.0.0.0 --port $PORT
+uvicorn app:app --host 0.0.0.0 --port $PORT --proxy-headers
